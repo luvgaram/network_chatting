abeac1fdcaf68046ab40f7cd96b7ac6363951115
Eunjoo Im <garam@nhnnext.org>
2014-11-14 14:52:09 +0900

    epoll_1개만

--- server.c
+++ server.c
@@ -12,14 +12,15 @@
 #define BUF_SIZE 1024
 #define PORT 3000
 #define IN_CURSOR 30 // 입력 커서 위치
-#define EPOLL_SIZE 10 // 동시 접속 클라이언트 수 
+#define LINE_SIZE 20 // 채팅줄 수
+#define EPOLL_SIZE 5 // epoll fd 수 
 
 void* send_msg(void* arg);
 void* recv_msg(void* arg);
 int kbhit(void);
 
 char msg[BUF_SIZE];
-int cursor = 20;
+int cursor = LINE_SIZE;
 
 int main()
 {
@@ -33,8 +34,7 @@ int main()
 	struct epoll_event evnt;
 	int ep_fd, evnt_cnt, i;
 
-
-//	pthread_t snd_thread, rcv_thread;
+	pthread_t snd_thread, rcv_thread;
 	void* thread_rtn;
 
 	if((serv_sock = socket (PF_INET, SOCK_STREAM, 0)) == -1) {
@@ -51,16 +51,21 @@ int main()
 		close(serv_sock);
 	}
 
-	if(str_len = listen(serv_sock, 1)) {
+	if(str_len = listen(serv_sock, EPOLL_SIZE)) {
 		perror("listen");
 		close(serv_sock);
 	}
+
+	ep_fd = epoll_create(EPOLL_SIZE);
+	ep_evnts = malloc(sizeof(struct epoll_event) * EPOLL_SIZE);
 
 	evnt.events = EPOLLIN;
 	evnt.data.fd = serv_sock;
 	epoll_ctl(ep_fd, EPOLL_CTL_ADD, serv_sock, &evnt);
 
-	while(1) {
+	printf("\033[2J");
+
+	while(1) {		
 		evnt_cnt = epoll_wait(ep_fd, ep_evnts, EPOLL_SIZE, -1); // 이벤트 발생까지 무한 대기
 		if(evnt_cnt == -1) {
 			perror("epoll_wait");
@@ -68,61 +73,43 @@ int main()
 		}
 
 		for(i = 0; i < evnt_cnt; i++) {
-			if(ep_evnts[i].data.fd == serv_sock) {
-				clnt_adr_sz = sizeof(clnt_adr);
-				clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
-				if(clnt_sock == -1) {
-					perror("accept");
-				}
-				evnt.events = EPOLLIN;
-				evnt.data.fd = clnt_sock;
-				epoll_ctl(ep_fd, EPOLL_CTL_ADD, clnt_sock, &evnt);
-				printf("%c[%d;%df", 0x1B, ((cursor) % 20) + 1, 0);
-				printf("new client connected : %d \n", clnt_sock);
-			
-//				pthread_create(&snd_thread, NULL, send_msg, (void*)&clnt_sock);
-//				pthread_create(&rcv_thread, NULL, recv_msg, (void*)&clnt_sock);
-//				pthread_join(snd_thread, &thread_rtn);
-//			 	pthread_join(rcv_thread, &thread_rtn);
+		if(ep_evnts[i].data.fd == serv_sock) {
+			clnt_adr_sz = sizeof(clnt_adr);
+			clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+			if(clnt_sock == -1) {
+				perror("accept");
 			}
-			else {
+			evnt.events = EPOLLIN;
+			evnt.data.fd = clnt_sock;
+			epoll_ctl(ep_fd, EPOLL_CTL_ADD, clnt_sock, &evnt);
+			printf("%c[%d;%df", 0x1B, ((cursor++) % LINE_SIZE) + 1, 25);
+			printf("new client connected : %d \n", clnt_sock);
+
+			pthread_create(&snd_thread, NULL, send_msg, (void*)&clnt_sock);
+			pthread_create(&rcv_thread, NULL, recv_msg, (void*)&clnt_sock);
+			pthread_join(snd_thread, &thread_rtn);
+		 	pthread_join(rcv_thread, &thread_rtn);
+		} else {
+			str_len = read(ep_evnts[i].data.fd, msg, BUF_SIZE);
+			if(str_len == 0) {
+			epoll_ctl(ep_fd, EPOLL_CTL_DEL, ep_evnts[i].data.fd, NULL);
+			close(ep_evnts[i].data.fd);
+			printf("%c[%d;%df", 0x1B, ((cursor) % LINE_SIZE) + 1, 0);
+			printf("client closed : %d \n", ep_evnts[i].data.fd);
+			} else {
 				str_len = read(ep_evnts[i].data.fd, msg, BUF_SIZE);
 				if(str_len == 0) {
+					pthread_detach(snd_thread);
+					pthread_detach(rcv_thread);
 					epoll_ctl(ep_fd, EPOLL_CTL_DEL, ep_evnts[i].data.fd, NULL);
-					close(ep_evnts[i].data.fd);
-					printf("%c[%d;%df", 0x1B, ((cursor) % 20) + 1, 0);
-					printf("client closed : %d \n", ep_evnts[i].data.fd);
-				} else {
-					str_len = read(ep_evnts[i].data.fd, msg, BUF_SIZE);
-					if(str_len == 0) {
-						epoll_ctl(ep_fd, EPOLL_CTL_DEL, ep_evnts[i].data.fd);
-					} else {
-//						pthread_detach(snd_thread);
-//						pthread_detach(rcv_thread);
-
-						recv_msg((void*)&clnt_sock);
-					}
-				}
+				} 
 			}
 		}
-
-//	if(clnt_sock == -1) {
-//		perror("accept");
-//		str_len = -1;
-//		close(serv_sock);
-//	}
-//	// 화면 지움
-//	printf("\033[2J");
-//
-//	// 쓰레드 생성
-//	pthread_create(&snd_thread, NULL, send_msg, (void*)&clnt_sock);
-//	pthread_create(&rcv_thread, NULL, recv_msg, (void*)&clnt_sock);
-//	pthread_join(snd_thread, &thread_rtn);
-//	pthread_join(rcv_thread, &thread_rtn);/	close(clnt_sock);
+		}
+	}
 	close(serv_sock);
 	close(ep_fd);
 	return 0;
-//	return str_len;
 }
 
 void* send_msg(void* arg)
@@ -141,7 +128,7 @@ void* send_msg(void* arg)
 			return NULL;;
 		}
 
-		printf("%c[%d;%df", 0x1B, ((cursor++) % 20) + 1, 25);
+		printf("%c[%d;%df", 0x1B, ((cursor++) % LINE_SIZE) + 1, 25);
 		printf("server: %s", msg);
 		write(clnt_sock, msg, strlen(msg));
 		printf("%c[%d;%df", 0x1B, IN_CURSOR, 0);
@@ -172,10 +159,9 @@ void* recv_msg(void* arg)
 		
 		if(rcv_len > 0) {
 			msg[rcv_len] = 0;
-
-			printf("%c[%d;%df", 0x1B, ((cursor) % 20) + 1, 0);
+			printf("%c[%d;%df", 0x1B, ((cursor) % LINE_SIZE) + 1, 0);
 			printf("client: %s", msg);
-			int i = IN_CURSOR - ((cursor % 20) + 2);
+			int i = IN_CURSOR - ((cursor % LINE_SIZE) + 2);
 			for(i; i > 0; i--) {
 				printf("%s", "\n");
 			}
@@ -184,3 +170,4 @@ void* recv_msg(void* arg)
 	}
 	return NULL;
 }
+
